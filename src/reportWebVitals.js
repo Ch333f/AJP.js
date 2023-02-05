b6e8212a4bb3dcfd5980c0972d63c35c38ec16bc
Adewale Joshua <73244523+mr-o-josh@users.noreply.github.com>
2023-02-05 21:44:18 +0100

    Pre production (#12)
    * Removed css specified as interpreter from shebang v0.1
    
    * Deleted license v0.1
    
    * Added homepage property v0.1
    
    * Created deployAJP.yml v0.1 (#5)
    
    * Changed chechout to checkout and updated deployAJP.yml v0.1 (#7)
    
    * Created deployAJP.yml v0.1
    
    * Updated deployAJP.yml v0.1
    
    * Changed chechout to checkout and updated deployAJP.yml v0.1
    
    * Fixed bug v0.1 (#8)
    
    * Created deployAJP.yml v0.1
    
    * Updated deployAJP.yml v0.1
    
    * Changed chechout to checkout and updated deployAJP.yml v0.1
    
    * Fixed bug v0.1
    
    * Update node version to 16 v0.1 (#9)
    
    * Created deployAJP.yml v0.1
    
    * Updated deployAJP.yml v0.1
    
    * Changed chechout to checkout and updated deployAJP.yml v0.1
    
    * Fixed bug v0.1
    
    * Update node version to 16 v0.1
    
    * Staging (#11)
    
    * Created deployAJP.yml v0.1
    
    * Updated deployAJP.yml v0.1
    
    * Changed chechout to checkout and updated deployAJP.yml v0.1
    
    * Fixed bug v0.1
    
    * Update node version to 16 v0.1
    
    * Updated v0.1

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,37 @@
+/**
+*!/usr/bin/env node
+* -*- coding: utf-8 -*-
+* Adewale Joshua Portfolio (AJP) App
+*/
+
+
+/**
+* author: OTechCup
+* copyright: Copyright 2022 - 2022, OTechCup
+* credits: ["Mr. O"]
+* version: process.env.AJP_VERSION
+* maintainer: OTechCup
+* email: support@exfac.info
+* status: process.env.AJP_ENVIRONMENT_STATUS
+*/
+
+
+// import modules
+
+
 const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+        import("web-vitals").then((
+            { getCLS, getFID, getFCP, getLCP, getTTFB }
+        ) => {
+            getCLS(onPerfEntry);
+            getFID(onPerfEntry);
+            getFCP(onPerfEntry);
+            getLCP(onPerfEntry);
+            getTTFB(onPerfEntry);
+        });
+    }
 };
+
 
 export default reportWebVitals;
